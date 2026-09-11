5d40c06f6387fe4cc77b15b310c413bee5455cf1
Saad <evryonelovechemicalengineering@gmail.com>
2026-09-11 11:18:45 +0500

    chore: remove leftover debug scripts

--- replace2.js
+++
@@ -1,5 +0,0 @@
-﻿const fs = require('fs');
-const file = 'src/app/claude-skills/ClaudeSkillsClient.tsx';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace(/\\/ \\/>/g, '/>');
-fs.writeFileSync(file, content);
